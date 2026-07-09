95a0260d414d4e18c89017874982b5d03f7fe335
Doug Jaser <136849311+Djaser23@users.noreply.github.com>
2026-07-08 19:01:10 -0700

    Update 05_readmission to include third query

--- queries/05_patient_readmission_analysis.sql
+++ queries/05_patient_readmission_analysis.sql
@@ -1,6 +1,8 @@
--- 30 Day Readmission Analysis
--- Includes single day intervals since discharge
--- This may capture planned transfers
+/*
+30 Day Readmission Analysis
+Includes single day intervals since discharge
+This may capture planned transfers
+*/
 WITH CTE AS (
 SELECT DESYNPUF_ID, CLM_ID, CLM_ADMSN_DT, NCH_BENE_DSCHRG_DT,
 LEAD(CLM_ADMSN_DT) OVER (PARTITION BY DESYNPUF_ID ORDER BY CLM_ADMSN_DT) AS next_admission
@@ -16,8 +18,11 @@ AND
 DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) > 0
 ORDER BY days_since_discharge;
 
--- Query 2 
--- Total readmission count including 1-day gaps
+/*
+Query 2 
+Total readmission count including 1-day gaps
+*/
+
 WITH CTE AS (
 SELECT DESYNPUF_ID, CLM_ID, CLM_ADMSN_DT, NCH_BENE_DSCHRG_DT,
 LEAD(CLM_ADMSN_DT) OVER (PARTITION BY DESYNPUF_ID ORDER BY CLM_ADMSN_DT) AS next_admission
@@ -28,3 +33,26 @@ WHERE
 DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) <= 30 
 AND 
 DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) > 0;
+
+/* 
+Query #3
+30 Day Readmission Analysis
+Excludes single day intervals since discharge
+This may fail to capture actual single day readmissions 
+in favor of avoiding inclusion of planned transfers 
+*/ 
+
+WITH CTE AS (
+SELECT DESYNPUF_ID, CLM_ID, CLM_ADMSN_DT, NCH_BENE_DSCHRG_DT,
+LEAD(CLM_ADMSN_DT) OVER (PARTITION BY DESYNPUF_ID ORDER BY CLM_ADMSN_DT) AS next_admission
+FROM inpatient_claims)
+
+SELECT DESYNPUF_ID, NCH_BENE_DSCHRG_DT, next_admission, 
+DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) 
+AS days_since_discharge
+FROM CTE
+WHERE
+DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) <=30 
+AND 
+DATEDIFF(STR_TO_DATE(next_admission, '%Y%m%d'), STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) > 1
+ORDER BY days_since_discharge;
