dbe4fa76a38fef0a72d527dadad1c4fdda7e319b
Doug Jaser <136849311+Djaser23@users.noreply.github.com>
2026-07-13 12:57:57 -0700

    Update 06b_readmission_rate_by_drg.sql
    Update comments to reflect the query

--- queries/06b_readmission_rate_by_drg.sql
+++ queries/06b_readmission_rate_by_drg.sql
@@ -1,18 +1,28 @@
 /*
 30 Day Readmission Rate by DRG
 Includes single day intervals since discharge
-This may capture planned transfers
+This may capture planned transfers.
+This query filters out discharges within the last 30 days of the data
+to avoid including rows where a 30 day readmission rate is impossible.
+The reason for this filter it to ensure a more accurate readmission rate which
+uses total admissions in its calculation.
+
+Volume thresholds for rate accuracy have been accounted for by filtering out
+rates where the total number of admissions is less than ten or 1 minus the readmission 
+rate is less than ten - a practice that reflects the use of the Central Limit Theorem 
+in the context of rates.
+
+
+HAVING readmission_rate * total_admissions >=10 AND -- filters out statistically unrealiable rates
+total_admissions * (1 - readmission_rate) >= 10 
+
+These DRG grouped readmission rates are ready for scrutiny given the acknowledgement
+that single day interval readmissions are included which may have been planned transfers.
+
+A mapping of DRG to expected length of stay is appropriate to determine where 
+inefficiencies may exist within the dataset.
 */
 
-/* 
-
-
--- PREVIOUS
-WITH censored_data_filter AS (
-SELECT MAX(STR_TO_DATE(NCH_BENE_DSCHRG_DT, '%Y%m%d')) - 30 AS adj_max_discharge 
-FROM inpatient_claims)
-
-*/
 
 WITH censored_data_filter AS (
 SELECT
@@ -47,17 +57,9 @@ SELECT
     COUNT(*) AS total_admissions
 FROM CTE3
 GROUP BY CLM_DRG_CD
-HAVING readmission_rate * total_admissions >=10 AND -- filters out statistically unrealiable rates
+HAVING readmission_rate * total_admissions >=10 AND -- filters out statistically unreliable rates
 total_admissions * (1 - readmission_rate) >= 10 
 ORDER BY readmission_rate DESC
 
 
-/*
-Next up:
-address data censoring with max date = done but needs writeup
-write the limitation
-consider low volume threshold for total admissions = updated query with HAVING condition 
-to display only statistically significant readmission rates with CLT formula 
-review drg readmission rates given these limitations
-*/
 
